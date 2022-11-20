8b27d40b6d5762978927da0d48d53698a7905870
xuxi <2551254474@qq.com>
2022-11-20 13:34:39 +0800

    前端初始化

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">新增</Button>
+  </div>
+);
+
+export default App;
